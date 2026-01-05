152fa0a81f87b114c1f17a37daec752eacba251f
Amanie SAID <amanie.said@etu.imt-nord-europe.fr>
2026-01-05 14:38:05 +0100

    1er commit

---
+++ src/main/java/com/imt/demo/JonkBackApplication.java
@@ -0,0 +1,13 @@
+package com.imt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonkBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonkBackApplication.class, args);
+	}
+
+}
