c16fc381c7a725d664b89567324fe58efb2f3090
Quentin Hamitouche <quentin.hamitouche@etu.imt-nord-europe.fr>
2026-01-09 00:38:11 +0100

    feat: implement toggling admin role

---
+++ src/main/java/com/imt/demo/dto/SetAdminRequest.java
@@ -0,0 +1,16 @@
+package com.imt.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetAdminRequest {
+    private boolean isAdmin;
+}
--- src/main/java/com/imt/demo/keycloak/KeycloakService.java
+++ src/main/java/com/imt/demo/keycloak/KeycloakService.java
@@ -1,16 +1,24 @@
 package com.imt.demo.keycloak;
 
 import com.imt.demo.dto.UserDto;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.InternalServerErrorException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.RoleScopeResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KeycloakService {
     private final Keycloak keycloak;
@@ -28,7 +36,7 @@ public class KeycloakService {
             ClientRepresentation first = realm.clients().findByClientId(properties.getClientId()).getFirst();
 
             if (first == null) {
-                throw new RuntimeException("Could not find client " + properties.getClientId());
+                throw new InternalServerErrorException("Could not find client " + properties.getClientId());
             }
 
             clientUUID = first.getId();
@@ -50,5 +58,34 @@ public class KeycloakService {
 
     private RealmResource getRealm() {
         return keycloak.realm(properties.getRealm());
+    }
+
+    public void setAdminStatus(String id, boolean adminStatus, String undertakingAdmin) {
+        log.info("User '{}' is setting user '{}' admin status to {}", undertakingAdmin, id, adminStatus);
+
+        UserResource userResource = getRealm().users().get(id);
+
+        if (userResource == null) {
+            throw new BadRequestException("Could not find user " + id);
+        }
+
+        RoleScopeResource roleScopeResource = userResource.roles().clientLevel(findClientUUID());
+
+        if (adminStatus) {
+            Optional<RoleRepresentation> foundAdminRole = findAdminRole(roleScopeResource.listAvailable());
+
+            foundAdminRole.ifPresent(adminRole -> roleScopeResource.add(List.of(adminRole)));
+        } else {
+            Optional<RoleRepresentation> foundAdminRole = findAdminRole(roleScopeResource.listEffective());
+
+            foundAdminRole.ifPresent(adminRole -> roleScopeResource.remove(List.of(adminRole)));
+        }
+    }
+
+    private Optional<RoleRepresentation> findAdminRole(List<RoleRepresentation> roles) {
+        return roles
+                .stream()
+                .filter(role -> role.getName().equals(KeycloakUserMapper.ADMIN))
+                .findFirst();
     }
 }
