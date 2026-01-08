36eaabf8cf75a6caac4f22cea4b2f744939e19fa
Quentin Hamitouche <quentin.hamitouche@etu.imt-nord-europe.fr>
2026-01-08 15:27:21 +0100

    feat: add keycloak client

---
+++ src/main/java/com/imt/demo/config/KeycloakAdminConfig.java
@@ -0,0 +1,26 @@
+package com.imt.demo.config;
+
+import com.imt.demo.keycloak.KeycloakProperties;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class KeycloakAdminConfig {
+    private final KeycloakProperties properties;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(properties.getServerUrl())
+                .realm(properties.getRealm()) // The realm where the admin user exists
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(properties.getClientId())
+                .clientSecret(properties.getClientSecret())
+                .build();
+    }
+}
