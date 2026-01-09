3777b4072ec4124d29d4550984dff70827c24de5
Quentin Hamitouche <quentin.hamitouche@etu.imt-nord-europe.fr>
2026-01-09 10:13:26 +0100

    feat: implement project retrieval

--- src/main/java/com/imt/demo/controller/ProjectController.java
+++ src/main/java/com/imt/demo/controller/ProjectController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,12 @@ public class ProjectController {
     @PreAuthorize("hasAnyRole('ADMIN', 'DEV')")
     public List<ProjectDto> getProjects() {
         return projectService.getProjects();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'DEV')")
+    public ProjectDto getProject(@PathVariable String id) {
+        return projectService.getProject(id);
     }
 
     @PostMapping
